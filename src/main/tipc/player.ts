73ebc1097657c8d328ba7b25354d5a7705d94733
suemor233 <suemor233@outlook.com>
2024-10-12 14:56:18 +0800

    chore: remove unused deps

--- src/main/tipc/player.ts
+++ src/main/tipc/player.ts
@@ -1,14 +1,9 @@
 import { dialog } from 'electron'
 
-import Ffmpeg from '../lib/ffmpeg'
 import { t } from './_instance'
 
 export const playerRoute = {
   showErrorDialog: t.procedure
     .input<{ title: string; content: string }>()
-    .action(async ({ input }) => dialog.showErrorBox(input.title, input.content)),
-  CompressedVideo: t.procedure.input<{ inputPath: string }>().action(async ({ input }) => {
-    const ffmpeg = new Ffmpeg(input.inputPath)
-    ffmpeg.run()
-  }),
+    .action(async ({ input }) => dialog.showErrorBox(input.title, input.content))
 }
