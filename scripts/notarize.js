5bc0abe15295e1f7b6a75ced6036def4a6950604
suemor233 <suemor233@outlook.com>
2024-11-21 13:18:41 +0800

    fix: ci

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -13,7 +13,8 @@ export default async function notarizing(context) {
   const appleIdPassword = process.env.APPLE_ID_PASSWORD
   const ascProvider = process.env.APPLE_ASC_PROVIDER
   const teamId = process.env.APPLE_TEAM_ID
-
+  // eslint-disable-next-line no-console
+  console.log(appBundleId, '=======')
   if (!appBundleId || !appleId || !appleIdPassword || !ascProvider || !teamId) {
     return
   }
